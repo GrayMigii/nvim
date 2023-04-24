69fa109bb22b3ad23d1f303895b1ae00eb08aa81
graymigii <abdullahkassa@gmail.com>
2023-04-24 18:37:11 +0200

    done

--- lua/gray/plugins/lsp/mason.lua
+++ lua/gray/plugins/lsp/mason.lua
@@ -19,18 +19,10 @@ end
 -- enable mason
 mason.setup()
 
-mason_lspconfig.setup({
+mason_lspconfig.setup{
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
-})
+}
 
-mason_null_ls.setup({
-  -- list of formatters & linters for mason to install
-  ensure_installed = {
-    "prettier", -- ts/js formatter
-    "stylua", -- lua formatter
-    "eslint_d", -- ts/js linter
-  },
-  -- auto-install configured formatters & linters (with null-ls)
-  automatic_installation = true,
-})
+mason_null_ls.setup{
+}
